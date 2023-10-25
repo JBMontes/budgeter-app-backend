8b4435098c95c85b15756db59a9540165cf06b85
Jeremy Montes <jeremymontes@pursuit.org>
2023-10-25 08:59:31 -0400

    updated the data in models folder

--- Models/data.js
+++ Models/data.js
@@ -3,34 +3,34 @@ module.exports = [
         id: 1111,
         item_name: "Banana",
         amount: 2,
-        date: "12/12/2023",
+        date: "09/12/2023",
         from: "Grocery Store",
         category: "Food"
     },
 
     {
         id: 2222,
-        item_name: "Banana",
-        amount: 2,
-        date: "12/12/2023",
-        from: "Grocery Store",
-        category: "Food"
+        item_name: "Rolex",
+        amount: 3000,
+        date: "10/12/2023",
+        from: "Rolex Inc.",
+        category: "Watch"
     },
 
     {
         id: 3333,
-        item_name: "Banana",
-        amount: 2,
-        date: "12/12/2023",
-        from: "Grocery Store",
-        category: "Food"
+        item_name: "Gas",
+        amount: 60,
+        date: "08/12/2023",
+        from: "Gas Station",
+        category: "Vehicle"
     },
     {
         id: 4444,
-        item_name: "Banana",
-        amount: 2,
-        date: "12/12/2023",
-        from: "Grocery Store",
-        category: "Food"
+        item_name: "Gym Membership",
+        amount: 20,
+        date: "07/12/2023",
+        from: "Planet Fitness",
+        category: "Health & Fitness"
     }
 ]
