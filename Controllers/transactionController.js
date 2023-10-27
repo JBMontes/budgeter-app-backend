1c5834ddf1e0b03462967169a334c0b713b68a08
Jeremy Montes <jeremymontes@pursuit.org>
2023-10-26 22:40:42 -0400

    Edited post state in controller

--- Controllers/transactionController.js
+++ Controllers/transactionController.js
@@ -1,117 +1,111 @@
-const express  = require('express');
+const express = require("express");
 const transactions = express.Router();
-const transactionArray = require('../Models/data');
+const transactionArray = require("../Models/data");
 transactions.use(express.json());
 
 //Renders All Items from Data
-transactions.get('/' , (req, res, next)=>{
-
-    try{
-
-        if(transactionArray){ 
-
-            res.status(200).json(transactionArray)
-
-        }else{
-
-            res.status(404).send({message: `Transactions not found!`});
-        }}
-
-    catch(error){
-        next(error)
+transactions.get("/", (req, res, next) => {
+  try {
+    if (transactionArray) {
+      res.status(200).json(transactionArray);
+    } else {
+      res.status(404).send({ message: `Transactions not found!` });
     }
+  } catch (error) {
+    next(error);
+  }
 });
 
 //Renders Specified Item by ID
-transactions.get('/:id', (req, res, next)=>{
-    const id = req.params.id;
-    const purchase = transactionArray.find((transaction)=> transaction.id === parseInt(id));
-    
-   try{ 
+transactions.get("/:id", (req, res, next) => {
+  const id = req.params.id;
+  const purchase = transactionArray.find(
+    (transaction) => parseInt(transaction.id) === parseInt(id)
+  );
 
-    if(purchase){ 
-
-        res.status(202).json(purchase) }
-
-    else{
-    res.status(404).send({ message: "Transaction not found!"})
-
-}}
-    catch(error){
-        next(error)
+  try {
+    if (purchase) {
+      res.status(202).json(purchase);
+    } else {
+      res.status(404).send({ message: "Transaction not found!" });
     }
+  } catch (error) {
+    next(error);
+  }
 });
 
 //Adds New Transaction to List
-transactions.post('/', (req, res, next)=>{
-    try{
-        const transactionBody = req.body;
-        if(transactionBody){
-            transactionArray.push(transactionBody);
-            res.status(201).send({message: `Transaction successfully added!`})
-        }else{
-            res.status(404).send({message: `Transaction not added!`})
-        }
+transactions.post("/", (req, res, next) => {
+  try {
+    const transactionBody = req.body;
+    if (transactionBody) {
+      transactionArray.push(transactionBody);
+      res.status(201).send({ message: `Transaction successfully added!` });
+    } else {
+      res.status(404).send({ message: `Transaction not added!` });
     }
-
-    catch(error){
-        next(error)
-    }
+  } catch (error) {
+    next(error);
+  }
 });
 
-//Updates a Transaction in the List
-transactions.put('/:id', (req,res, next)=>{
-try{
+// Updates a Transaction in the List
+// transactions.put('/:id', (req,res, next)=>{
+// try{
 
-const transactionID = parseInt(req.params.id);
-const transactionToUpdate = req.body;
-const transactionIndex = transactionArray.findIndex((item)=>item.id === transactionID)
+// const transactionID = parseInt(req.params.id);
+// const transactionToUpdate = req.body;
+// const transactionIndex = transactionArray.findIndex((item)=>item.id === transactionID)
 
-if(transactionIndex === -1){
-    res.status(404).send({message: `Transaction not found!`});
-}
+// if(transactionIndex === -1){
+//     res.status(404).send({message: `Transaction not found!`});
+// }
 
-//The existing transaction that's being updated
-const chosenTransaction = transactionArray[transactionIndex]; 
+// //The existing transaction that's being updated
+// const chosenTransaction = transactionArray[transactionIndex];
 
-for(let key in transactionToUpdate){
-    if(chosenTransaction.hasOwnProperty([key])){
-//Transfers the user's inputs to the existing transaction
-        chosenTransaction[key] = transactionToUpdate[key];
-    }
-}
-//Transfers the new details to the position of the existing transaction
-transactionArray[transactionIndex] = chosenTransaction;
+// for(let key in transactionToUpdate){
+//     if(chosenTransaction[key]){
+// //Transfers the user's inputs to the existing transaction
+//         chosenTransaction[key] = transactionToUpdate[key];
+//     }
+// }
+// //Transfers the new details to the position of the existing transaction
+// transactionArray[transactionIndex] = chosenTransaction;
 
-res.status(200).send(chosenTransaction);
+// res.status(200).send(transactionArray);
 
-}
+// }
 
-catch(error){
+// catch(error){
 
-    next(error)
-}
+//     next(error)
+// }
+// });
+
+transactions.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const foundIndex = transactionArray.findIndex((element) => parseInt(element.id) === parseInt(id));
+  transactionArray[foundIndex] = req.body;
+  res.send(transactionArray);
 });
-
 
 //Deletes a Transaction
-transactions.delete('/:id', (req, res, next) => {
-   
-    try {
-        const id = parseInt(req.params.id);
-        const itemIndex = transactionArray.findIndex(item => item.id === id);
+transactions.delete("/:id", (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
+    const itemIndex = transactionArray.findIndex((item) => item.id === id);
 
-        if (itemIndex === -1) {
-            return res.status(404).send({ message: 'Item not found' });
-        }
-
-        const deletedItem = transactionArray.splice(itemIndex, 1); 
-
-        res.send(deletedItem[0]);
-
-    } catch (error) {
-        next(error);
+    if (itemIndex === -1) {
+      return res.status(404).send({ message: "Item not found" });
     }
+
+    const deletedItem = transactionArray.splice(itemIndex, 1);
+
+    res.send(deletedItem[0]);
+  } catch (error) {
+    next(error);
+  }
 });
 
-module.exports = transactions
+module.exports = transactions;
